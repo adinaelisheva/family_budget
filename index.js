c8a2dcd455cc7c7aed2bc988156aa0bca7d378b1
Adina Rubinoff <adinarubinoff@gmail.com>
2016-07-11 21:51:25 -0400

    some more updates. basic remaining amounts work

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 angular.module('budget', []).controller('budgetCtrl', ['$scope', function($scope) {
-  $scope.categories = ["cat", "food", "shopping", "etc"];
+  $scope.categories = [
+    {name:"cat", budget: 50},
+    {name: "food", budget: 300},
+    {name: "shopping", budget: 50},
+    {name: "etc", budget: 30}
+  ];
 
   $scope.monthlyData = [
-    {item: "Snickers", date: "07/01/16", category: "cat", amount: 100},
-    {item: "books", date: "07/04/16", category: "etc", amount: 24},
-    {item: "groceries", date: "07/10/16", category: "food", amount: 100}
+    {name: "Snickers", date: "07/01/16", category: "cat", value: 100},
+    {name: "books", date: "07/04/16", category: "etc", value: 24},
+    {name: "groceries", date: "07/10/16", category: "food", value: 100}
   ];
   
   $scope.submitbutt = function(){
-    alert($scope.newDate+", "+$scope.newItem+", "+$scope.newCategory+", "+$scope.newAmount); 
+    alert($scope.newDate+", "+$scope.newName+", "+$scope.newCategory+", "+$scope.newValue); 
   }
   $scope.transferbutt = function(){
     alert($scope.transferFrom+", "+$scope.transferTo+", "+$scope.transferAmount); 
   }
+  
+  $scope.getRemaining = function(category) {
+    var remaining = 0;
+    for(var i = 0; i < $scope.categories.length; i++) {
+      if ($scope.categories[i].name === category) {
+        remaining = $scope.categories[i].budget;
+        break;
+      }
+    }
+    
+    for(var i = 0; i < $scope.monthlyData.length; i++) {
+      if($scope.monthlyData[i].name === category) {
+        remaining -= $scope.monthlyData[i].amount;
+      }
+    }
+    
+    return remaining;
+  
+  };
+  
+  $scope.getRemainingStyle = function() {
+    return "{color: red}"
+  }
+  
   
 }]);
