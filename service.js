5742a955add324d0bf46b675a8399853925dd121
Adina Rubinoff <adinarubinoff@gmail.com>
2016-09-05 17:41:36 -0400

    Using promises and a service to handle all the http stuff.
    Fixes #12

---
+++ service.js
@@ -0,0 +1,46 @@
+angular.module('budget',[]).service('httpSrvc', ['$http', function ($http) {
+  
+  var data = {
+    categories: [],
+    monthly: [],
+    yearly: []
+  };
+  
+  this.fetchAllData = function() {
+    return $http.get('/family-budget/categories.php').then(function(cats) {
+      data.categories = cats.data;
+    }).then(function() {
+      return $http.get('/family-budget/month.php');
+    }).then(function(months) {
+      data.monthly = months.data;
+    }).then(function() {
+      return $http.get('/family-budget/year.php');
+    }).then(function(years) {
+      data.yearly = years.data;
+    });
+  };
+  
+  this.submit = function(date, name, category, value) {
+    if(!date || !name || !category || !value) { return; }
+    
+    return $http.post('/family-budget/add.php', {
+      'cat': category,
+      'name': name,
+      'value': value,
+      'date': date
+    });
+  }
+  
+  this.transfer = function(transferFrom, transferTo, amt) {
+    if(!transferFrom || !transferTo || !amt) { return; }
+    return $http.post('/family-budget/transfer.php', {
+      'catin': transferTo,
+      'catout': transferFrom,
+      'value': amt
+    })
+  }
+  
+  this.data = data;
+    
+
+}]);
