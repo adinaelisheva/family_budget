b10aac815ddcb525ab8d7c715ee29d37372dfd20
Adina Rubinoff <adina.elisheva@gmail.com>
2019-09-01 18:30:19 -0400

    update php call

--- service.js
+++ service.js
@@ -6,11 +6,18 @@ angular.module('budget',[]).service('httpSrvc', ['$http', function ($http) {
     yearly: []
   };
   
-  this.fetchAllData = function() {
+  this.fetchAllData = function(month, year) {
     return $http.get('/family-budget/categories.php').then(function(cats) {
       data.categories = cats.data;
     }).then(function() {
-      return $http.get('/family-budget/month.php');
+      let qStr = '/family-budget/month.php?';
+      if (month != undefined) {
+        qStr += `month=${month}&`;
+      }
+      if (year) {
+        qStr += `year=${year}&`;
+      }
+      return $http.get(qStr);
     }).then(function(months) {
       data.monthly = months.data;
     }).then(function() {
